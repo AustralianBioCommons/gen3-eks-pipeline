8f491e4dea15d49f5ade27db9767ada57f8a3409
Guerdon Mukama <guerdon@biocommons.org.au>
2024-03-06 19:47:41 +1100

    update to m5.2xlarge node types

--- lib/cluster/index.ts
+++ lib/cluster/index.ts
@@ -349,26 +349,26 @@ export function testClusterProvider(clusterName: string) {
 export function uatClusterProvider(clusterName: string) {
     const version = KubernetesVersion.V1_28;
     return new blueprints.GenericClusterProvider({
-      version: version,
-      clusterName: clusterName,
-      endpointAccess: EndpointAccess.PRIVATE,
-      managedNodeGroups: [
-        {
-          id: "mng1",
-          minSize: 1,
-          maxSize: 4,
-          desiredSize: 3,
-          instanceTypes: [new ec2.InstanceType("m5.large")],
-          amiType: NodegroupAmiType.AL2_X86_64,
-          nodeGroupCapacityType: CapacityType.ON_DEMAND,
-          amiReleaseVersion: "1.28.5-20240110",
-          tags: {
-            Name: "GEN3 Cluster",
-            Type: "ACDC",
-            ENV: "uat",
-          },
-        },
-      ],
+        version: version,
+        clusterName: clusterName,
+        
+        managedNodeGroups: [
+            {
+                id: 'mng1',
+                minSize: 1,
+                maxSize: 4,
+                desiredSize: 3,
+                instanceTypes: [new ec2.InstanceType('m5.large')],
+                amiType: NodegroupAmiType.AL2_X86_64,
+                nodeGroupCapacityType: CapacityType.ON_DEMAND,
+                amiReleaseVersion: '1.28.5-20240110',
+                tags: {
+                    Name: 'GEN3 Cluster',
+                    Type: 'ACDC',
+                    ENV: 'uat'
+                },
+            },
+        ],
     });
 }
 
@@ -381,6 +381,21 @@ export function prodClusterProvider(clusterName: string) {
     managedNodeGroups: [
       {
         id: "mng1",
+        minSize: 1,
+        maxSize: 3,
+        desiredSize: 3,
+        instanceTypes: [new ec2.InstanceType("m5.large")],
+        amiType: NodegroupAmiType.AL2_X86_64,
+        nodeGroupCapacityType: CapacityType.ON_DEMAND,
+        amiReleaseVersion: "1.28.5-20240110",
+        tags: {
+          Name: "GEN3 Cluster",
+          Type: "ACDC",
+          ENV: "prod",
+        },
+      },
+      {
+        id: "mng2",
         minSize: 1,
         maxSize: 4,
         desiredSize: 3,
