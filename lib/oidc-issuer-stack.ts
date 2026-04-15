cf3bbae8b115fb656642c2dae685117e95ed3c99
Guerdon Mukama <gmukama@gmail.com>
2026-04-15 12:41:20 +1000

    fix: argocd config conflict

--- lib/oidc-issuer-stack.ts
+++ lib/oidc-issuer-stack.ts
@@ -64,7 +64,7 @@ export class OidcIssuerStack extends cdk.Stack {
           this.formatArn({
             service: "ssm",
             resource: "parameter",
-            resourceName: `/gen3/${envKey}/*`,
+            resourceName: `gen3/*`,
           }),
         ],
       })
