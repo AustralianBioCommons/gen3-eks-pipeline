f8d0b774c6ffcd09e815761ca69330f57e6ec26a
Guerdon Mukama <gmukama@gmail.com>
2026-07-07 16:20:32 +1000

    fix: bug fix

--- lib/oidc-issuer-stack.ts
+++ lib/oidc-issuer-stack.ts
@@ -19,13 +19,13 @@ export interface OidcIssuerStackProps extends cdk.StackProps {
 
 export class OidcIssuerStack extends cdk.Stack {
   public readonly oidcIssuer: string;
-  public readonly env: cdk.Environment;
+  // public readonly env: cdk.Environment;
 
   constructor(scope: Construct, id: string, props: OidcIssuerStackProps) {
     super(scope, id, props);
 
     const { clusterName, oidcIssuerParameter } = props;
-    this.env = props.env
+    // this.env = props.env
 
     const envKey = `${props.namespace}-${props.clusterName}`;
 
